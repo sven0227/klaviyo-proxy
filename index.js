52595aa34358a23d7652c433f249c110f28c5b15
App God <phillymantis@gmail.com>
2023-04-22 01:42:56 -0400

    initial

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+const rateLimit = require('express-rate-limit')
+require('dotenv').config()
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10
+    max: 100
+})
+
+app.use(limiter)
+app.set('trust proxy',1)
+
+// Routes
+app.use('/api', require('./routes'))
+
+//Enable cors
+app.use(cors())
+
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
